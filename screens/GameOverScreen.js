30ff946e287bd27b5e712683761b360c85da8286
EAD\rnair63 <rnair63@dxc.com>
2021-08-18 14:48:32 +0530

    addded images local/network

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import BodyText from "../components/BodyText";
 import Colors from "../constants/color";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <BodyText>Game Over!</BodyText>
+      <BodyText style={styles.title}>Game Over!</BodyText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          fadeDuration={300}
+          resizeMode="cover"
+          // source={require("../assets/success.png")}
+          source={{
+            uri: "https://t3.ftcdn.net/jpg/02/51/28/40/360_F_251284036_cU2F5jsod18MupuAAZDNtviJ68TCvLjN.jpg",
+          }}
+        />
+      </View>
       <BodyText>Number of rounds: {props.rounds}</BodyText>
       <BodyText>Number: {props.number}</BodyText>
       <Button title="Restart" color={Colors.primary} onPress={props.newGame} />
@@ -20,6 +31,24 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingVertical: 10,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    marginVertical: 30,
+    // borderColor: "black",
+    // borderWidth: 5,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    // borderRadius: 200,
   },
 });
 export default GameOverScreen;
